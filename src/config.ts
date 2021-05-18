3c20a37a6215b73190820348c463cc09e6b672e7
Arthur Matias <ahmmfdc@gmail.com>
2021-05-18 15:32:15 -0300

    config

---
+++ src/config.ts
@@ -0,0 +1,21 @@
+export const BLOCKCHAIN_WALLET_ADDRESS: string = "blockchain-wallet";
+
+export const DIFFICULTY: number = 3;
+
+//in milliseconds
+export const MINE_RATE: number = 3000;
+
+export const INITIAL_BALANCE: number = 500;
+
+export const MINING_REWARD: number = 10;
+
+//end points
+
+export const ENDPOINT_POST_MINE: string = "/mine";
+export const ENDPOINT_POST_TRANSACTIONS: string = "/transact";
+
+export const ENDPOINT_GET_BALANCE: string = "/balance";
+export const ENDPOINT_GET_BLOCKS: string = "/blocks";
+export const ENDPOINT_GET_MINE_TRANSACTIONS: string = "/mine-transactions"
+export const ENDPOINT_GET_PUBLIC_KEY: string = "/public-key";
+export const ENDPOINT_GET_TRANSACTIONS: string = "/transactions";
